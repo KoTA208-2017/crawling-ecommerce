04c258bc1b305d0cf2532a03ca264a99055c99e2
ProgNuha <mufida.nuha.tif17@polban.ac.id>
2020-05-17 00:25:50 +0700

    crawl a category

--- crawling_e_commerce/settings.py
+++ crawling_e_commerce/settings.py
@@ -64,12 +64,12 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-    'scrapy.pipelines.images.ImagesPipeline': 1
+# ITEM_PIPELINES = {
+#     'scrapy.pipelines.images.ImagesPipeline': 1
     # 'crawling_e_commerce.pipelines.CrawlingECommercePipeline': 300
     # 'scrapy.pipelines.files.FilesPipeline': 1
-}
-IMAGES_STORE = 'tmp/images/'
+# }
+# IMAGES_STORE = 'tmp/images/'
 # FILES_STORE = '/tmp/images/'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
--- crawling_e_commerce/spiders/mapemall_crawler.py
+++ crawling_e_commerce/spiders/mapemall_crawler.py
@@ -4,6 +4,7 @@ import csv
 import os
 import logging
 import time
+import urllib.request
 
 from selenium import webdriver
 from selenium.webdriver import Chrome
@@ -14,36 +15,20 @@ class MapemallCrawlerSpider(scrapy.Spider):
     name = 'mapemall_crawler'
     # separator = 'n/'
     allowed_domains = ['www.mapemall.com']
-    start_urls = ['https://www.mapemall.com/forher/clothing?ct=']
     options = webdriver.ChromeOptions()
     options.add_argument('window-size=1200x600')
 
     def __init__(self):
+        self.start_urls = ['https://www.mapemall.com/forher/clothing?ct=1-7-10-102']
         self.driver = webdriver.Chrome(chrome_options=MapemallCrawlerSpider.options)
-
-    def start_requests(self):
-        """Read category_text from categories file amd construct the URL"""
-
-        with open(os.path.join(os.path.dirname(__file__), "../resources/mapemall_categories.csv")) as categories:
-            for category in csv.DictReader(categories):
-                category_text=category["category"]
-                url=str(MapemallCrawlerSpider.start_urls[0])+category_text
-                # The meta is used to send our search text into the parser as metadata
-                yield scrapy.Request(url, callback = self.parse, meta = {"category_text": category_text})
 
     def parse(self, response):
         """Function to process clothes category results page"""
         self.driver.get(response.url)
-        product_category=response.meta["category_text"]
-        # products=response.xpath("//*[(@class='list-item')]")
-        
-        # item containers for storing product
-        items = CrawlingECommerceItem()
-        self.driver.get(response.request.url)
 
-        view_more = self.driver.find_element_by_xpath('//*[(@class="load-more")]//div[@class="load-btn"]//a')
-        # MapemallCrawlerSpider.scroll(self, self.driver, 5)
-        while True:
+        totalPages = int(self.driver.execute_script(" return document.getElementById('totalPages').value"))
+
+        for page in range(1,totalPages):
             view_more = '//*[(@class="load-more")]//div[not(contains(@style,"display:none"))]'
 
             try:
@@ -51,51 +36,48 @@ class MapemallCrawlerSpider(scrapy.Spider):
             except ElementNotInteractableException:
                 self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                 time.sleep(5)
-            else: 
-                yield response.follow(str(response.request.url), callback = self.parse, meta = {"category_text": product_category})
 
-        # products = self.driver.find_elements_by_xpath('//*[(@class="product-grid")]')
-        # source = self.driver.page_source
-        # print("!SOURCE "+source)
-        
+        products = self.driver.find_elements_by_xpath('//*[(@class="col-12-3 col-sm-12-6 list-item")]')
+
+        # item containers for storing product
+        items = CrawlingECommerceItem()
+
         # iterating over search results
-        # for product in products:
-        #     # Defining the XPaths
-        #     XPATH_PRODUCT_LINK=".//*[contains(concat( ' ', @class, ' ' ), concat( ' ', 'goods-tit', ' ' ))]//a"
-        #     XPATH_PRODUCT_NAME=".//div[@class='goods-introudce']//a/@href"
-        #     XPATH_PRODUCT_PRICE=".//div[@class='catalog-detail']//div[@class='detail-right']//p/text()"
-        #     XPATH_PRODUCT_IMAGE_LINK=".//img"
+        for product in products:
+            # Defining the XPaths
+            XPATH_PRODUCT_LINK='.//div[@class="goods-tit"]//a'
+            XPATH_PRODUCT_NAME='.//div[@class="goods-tit"]//a'
+            XPATH_PRODUCT_PRICE='.//div[@class="goods-price special-price"]//span'
+            XPATH_PRODUCT_IMAGE_LINK='.//div[@class="img-box"]/a/img'
 
-        #     raw_product_name=product.xpath(XPATH_PRODUCT_NAME).get()
-        #     raw_product_price=product.xpath(XPATH_PRODUCT_PRICE).get()
-        #     raw_product_image_link=product.xpath(XPATH_PRODUCT_IMAGE_LINK).extract()
-        #     raw_product_link=product.xpath(XPATH_PRODUCT_LINK).get()
+            raw_product_name=product.find_element_by_xpath(XPATH_PRODUCT_NAME).text
+            raw_product_price=product.find_element_by_xpath(XPATH_PRODUCT_PRICE).text
+            raw_product_image_link=product.find_element_by_xpath(XPATH_PRODUCT_IMAGE_LINK).get_attribute("src")
+            raw_product_link=product.find_element_by_xpath(XPATH_PRODUCT_LINK).get_attribute("href")
 
-    #         # cleaning the data
-    #         product_name=''.join(raw_product_name).strip(
-    #         ) if raw_product_name else None
-    #         product_price=''.join(raw_product_price).strip(
-    #         ) if raw_product_price else None
-    #         product_image_link=''.join(raw_product_image_link).strip(
-    #         ) if raw_product_image_link else None
-    #         product_link=''.join(raw_product_link).strip(
-    #         ) if raw_product_link else None
+            # cleaning the data
+            product_name=''.join(raw_product_name).strip(
+            ) if raw_product_name else None
+            product_price=''.join(raw_product_price).strip(
+            ) if raw_product_price else None
+            product_image_link=''.join(raw_product_image_link).strip(
+            ) if raw_product_image_link else None
+            product_link=''.join(raw_product_link).strip(
+            ) if raw_product_link else None
 
-    #         # storing item
-    #         yield CrawlingECommerceItem (
-    #             product_name=product_name,
-    #             product_price=product_price,
-    #             product_url=product_link,
-    #             product_category=product_category,
-    #             image_urls=raw_product_image_link
-    #         )
+            product_category=MapemallCrawlerSpider.select_category(self,url=response.request.url)
+            raw_product_image_link=MapemallCrawlerSpider.split_image_url(self,url=raw_product_image_link)
 
-    #         # yield items
-        
-    #     XPATH_PRAGINATION_LINK="//*[(@class='next right')]/a/@href"
+            # storing item
+            yield CrawlingECommerceItem (
+                product_name=product_name,
+                product_price=product_price,
+                product_url=product_link,
+                product_category=product_category,
+                image_urls=raw_product_image_link
+            )
 
         self.driver.close()
-        # yield response.follow(str(response.request.url), callback = self.parse, meta = {"category_text": product_category})
 
     def scroll(self, driver, timeout):
         scroll_pause_time = timeout
@@ -154,7 +136,7 @@ class MapemallCrawlerSpider(scrapy.Spider):
     def select_category(self, url):
         argument = MapemallCrawlerSpider.split_url(self,url = url)
         categories = MapemallCrawlerSpider.split_category(self,category=argument)
-
+        
         category = {
             '8': MapemallCrawlerSpider.select_category_top(self),
             '9': MapemallCrawlerSpider.select_category_top(self),
