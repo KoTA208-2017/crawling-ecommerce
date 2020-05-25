313df068b09f52c86170546d6dc0bd9f92a67f23
ProgNuha <mufida.nuha.tif17@polban.ac.id>
2020-05-25 10:44:53 +0700

    feat(product): add iterate products for extract data product

--- crawling_e_commerce/spiders/zalora_crawler.py
+++ crawling_e_commerce/spiders/zalora_crawler.py
@@ -26,3 +26,39 @@ class ZaloraCrawlerSpider(scrapy.Spider):
 
         # item containers for storing product
         items = CrawlingECommerceItem()
+
+        # iterating over search results
+        for product in products:
+            # Defining the XPaths
+            XPATH_PRODUCT_LINK='.//a[@class="b-catalogList__itmLink itm-link"]'
+            XPATH_PRODUCT_NAME='.//em'
+            XPATH_PRODUCT_PRICE='.//span[@class="b-catalogList__itmPrice special"]'
+            XPATH_PRODUCT_IMAGE_LINK='.//img[@class="b-catalogList__itm-img b-catalogList__itm-img"]'
+
+            raw_product_name = product.find_element_by_xpath(XPATH_PRODUCT_NAME).text
+            raw_product_price = product.find_element_by_xpath(XPATH_PRODUCT_PRICE).text
+            raw_product_image_link = product.find_element_by_xpath(XPATH_PRODUCT_IMAGE_LINK).get_attribute("src")
+            raw_product_link = product.find_element_by_xpath(XPATH_PRODUCT_LINK).get_attribute("href")
+            
+            print("!print" + raw_product_image_link)
+
+            # cleaning the data
+            product_name=''.join(raw_product_name).strip(
+            ) if raw_product_name else None
+            product_price=''.join(raw_product_price).strip(
+            ) if raw_product_price else None
+            product_image_link=''.join(raw_product_image_link).strip(
+            ) if raw_product_image_link else None
+            product_link=''.join(raw_product_link).strip(
+            ) if raw_product_link else None
+
+            # storing item
+            yield CrawlingECommerceItem (
+                site_name = 'Zalora',
+                product_name = product_name,
+                product_price = product_price,
+                product_url = product_link,
+                product_category = "category",
+                product_image_url = raw_product_image_link,
+                product_image = '.jpg'
+            )
