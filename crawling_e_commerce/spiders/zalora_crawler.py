b9a719a60ed2ee5cb7d99cc6736e8b4c60801222
ProgNuha <mufida.nuha.tif17@polban.ac.id>
2020-05-25 10:13:08 +0700

    build(webdriver): initial chrome webdriver

--- crawling_e_commerce/spiders/zalora_crawler.py
+++ crawling_e_commerce/spiders/zalora_crawler.py
@@ -5,26 +5,24 @@ import os
 import logging
 
 from ..items import CrawlingECommerceItem
+from selenium import webdriver
+from selenium.webdriver import Chrome
 
 class ZaloraCrawlerSpider(scrapy.Spider):
     name = 'zalora_crawler'
     allowed_domains = ['www.zalora.co.id']
-    start_urls = ['http://www.zalora.co.id/']
+    start_urls = ['http://www.zalora.co.id']
+    options = webdriver.ChromeOptions()
+    options.add_argument('window-size=1200x600')
 
-    def start_requests(self):
-        """Read category_text from categories file and construct the URL"""
-
-        with open(os.path.join(os.path.dirname(__file__), "../resources/zalora_categories.csv")) as categories:
-            for category in csv.DictReader(categories):
-                category_text=category["category"]
-                url=str(ZaloraCrawlerSpider.start_urls[0])+"/women/pakaian/?page=1&category_id="+category_text
-                # The meta is used to send our search text into the parser as metadata
-                yield scrapy.Request(url, callback = self.parse, meta = {"category_text": category_text})
+    def __init__(self):
+        self.start_urls = ['https://www.zalora.co.id/women/pakaian/?category_id=18']
+        self.driver = webdriver.Chrome(chrome_options=ZaloraCrawlerSpider.options)
 
     def parse(self, response):
         """Function to process clothes category results page"""
-        product_category=response.meta["category_text"]
-        products=response.xpath('//*[(@class="b-catalogList__itm hasOverlay unit size1of3")]')
+        self.driver.get(response.url)
+        products=self.driver.find_elements_by_xpath('//*[(@class="b-catalogList__itm hasOverlay unit size1of3")]')
 
         # item containers for storing product
         items = CrawlingECommerceItem()
