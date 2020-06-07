83b7668abfb880f089f29e14900f6335bf547c7e
ProgNuha <mufida.nuha.tif17@polban.ac.id>
2020-06-07 23:18:01 +0700

    refactor(berrybenka-spider): add whitespace

--- crawling_e_commerce/spiders/berrybenka_crawler.py
+++ crawling_e_commerce/spiders/berrybenka_crawler.py
@@ -17,17 +17,17 @@ class BerrybenkaSpider(scrapy.Spider):
 
         with open(os.path.join(os.path.dirname(__file__), "../resources/berrybenka_categories.csv")) as categories:
             for category in csv.DictReader(categories):
-                category_text=category["category"]
-                url=str(BerrybenkaSpider.start_urls[0])+"/clothing/"+category_text+"/women/0"
+                category_text = category["category"]
+                url = str(BerrybenkaSpider.start_urls[0])+"/clothing/"+category_text+"/women/0"
                 # The meta is used to send our search text into the parser as metadata
                 yield scrapy.Request(url, callback = self.parse, meta = {"category_text": category_text})
 
     def parse(self, response):
         """Function to process clothes category results page"""
         site_name = "Berrybenka"
-        product_category=response.meta["category_text"]
+        product_category = response.meta["category_text"]
         product_category = EcommerceItem.get_category(self, product_category, site_name)
-        products=response.xpath("//*[(@id='li-catalog')]")
+        products = response.xpath("//*[(@id='li-catalog')]")
         
         # item containers for storing product
         items = EcommerceItem()
@@ -35,42 +35,42 @@ class BerrybenkaSpider(scrapy.Spider):
         # iterating over search results
         for product in products:
             # Defining the XPaths
-            XPATH_PRODUCT_LINK=".//a/@href"
-            XPATH_PRODUCT_NAME=".//div[@class='catalog-detail']//div[@class='detail-left']//h1/text()"
-            XPATH_PRODUCT_PRICE=".//div[@class='catalog-detail']//div[@class='detail-right']//p/text()"
-            XPATH_PRODUCT_IMAGE_LINK=".//div[@class='catalog-image']//img/@src"
+            XPATH_PRODUCT_LINK = ".//a/@href"
+            XPATH_PRODUCT_NAME = ".//div[@class='catalog-detail']//div[@class='detail-left']//h1/text()"
+            XPATH_PRODUCT_PRICE = ".//div[@class='catalog-detail']//div[@class='detail-right']//p/text()"
+            XPATH_PRODUCT_IMAGE_LINK = ".//div[@class='catalog-image']//img/@src"
 
-            raw_product_name=product.xpath(XPATH_PRODUCT_NAME).get()
-            raw_product_price=product.xpath(XPATH_PRODUCT_PRICE).get()
-            raw_product_image_link=product.xpath(XPATH_PRODUCT_IMAGE_LINK).extract()
-            raw_product_link=product.xpath(XPATH_PRODUCT_LINK).get()
+            raw_product_name = product.xpath(XPATH_PRODUCT_NAME).get()
+            raw_product_price = product.xpath(XPATH_PRODUCT_PRICE).get()
+            raw_product_image_link = product.xpath(XPATH_PRODUCT_IMAGE_LINK).extract()
+            raw_product_link = product.xpath(XPATH_PRODUCT_LINK).get()
             
             # cleaning the data
-            product_name=''.join(raw_product_name).strip(
+            product_name = ''.join(raw_product_name).strip(
             ) if raw_product_name else None
-            product_price=''.join(raw_product_price).strip(
+            product_price = ''.join(raw_product_price).strip(
             ) if raw_product_price else None
-            product_image_link=''.join(raw_product_image_link).strip(
+            product_image_link = ''.join(raw_product_image_link).strip(
             ) if raw_product_image_link else None
-            product_link=''.join(raw_product_link).strip(
+            product_link = ''.join(raw_product_link).strip(
             ) if raw_product_link else None
 
             # split image link
             image_link = str(raw_product_image_link[0])
             template = "https://im.berrybenka.com/assets/cache/300x456/product-overlay/_VDEGZ_2836.png"
             if(image_link == template):
-                raw_product_image_link[0] = BerrybenkaSpider.split_url_image(self,raw_product_image_link[1])
+                raw_product_image_link[0] = BerrybenkaSpider.split_url_image(self, raw_product_image_link[1])
             else:
-                raw_product_image_link[0] = BerrybenkaSpider.split_url_image(self,raw_product_image_link[0])
+                raw_product_image_link[0] = BerrybenkaSpider.split_url_image(self, raw_product_image_link[0])
 
             # storing item
             yield EcommerceItem (
                 site_name = site_name,
-                product_name=product_name,
-                product_price=product_price,
-                product_url=product_link,
-                product_category=product_category,
-                image_urls=raw_product_image_link
+                product_name = product_name,
+                product_price = product_price,
+                product_url = product_link,
+                product_category = product_category,
+                image_urls = raw_product_image_link
             )
         
         XPATH_PRAGINATION_LINK="//*[(@class='next right')]/a/@href"
